5f64990c908b8dcb392aae85ec4278312ca94ac1
Amit-qa-eng <amitqaeng@gmail.com>
2025-08-08 15:25:37 +0530

    remove threadsleep and added webdriverwait

--- src/test/java/com/citysite/tests/LoginTest.java
+++ src/test/java/com/citysite/tests/LoginTest.java
@@ -1,5 +1,10 @@
 package com.citysite.tests;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -10,17 +15,20 @@ import com.citysite.pages.LoginPage;
 public class LoginTest  extends BaseTest{
 
 	 private LoginPage loginPage;
-
+	 private WebDriverWait wait;
+	 
+	 
 	    @BeforeMethod
 	    public void setupTest() {
 	        setup();
 	        loginPage = new LoginPage(driver);
+	        wait=new WebDriverWait(driver,Duration.ofSeconds(10));
 	    }
 
 	    @Test(priority = 1)
 	    public void testJobSeekerLogin() throws InterruptedException {
 	        loginPage.openJobSeekerLogin();
-	        Thread.sleep(1000); // Can be replaced with WebDriverWait
+	        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
 	        loginPage.enterEmail("testdeveloper30@gmail.com");
 	        loginPage.enterPassword("Test#123?");
 	        loginPage.checkRememberMe();
@@ -31,7 +39,7 @@ public class LoginTest  extends BaseTest{
 	    @Test(priority = 2)
 	    public void testRecruiterLogin() throws InterruptedException {
 	        loginPage.openRecruiterLogin();
-	        Thread.sleep(1000); // Can be replaced with WebDriverWait
+	        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
 	        loginPage.enterEmail("testdeveloper30@gmail.com");
 	        loginPage.enterPassword("Test#123?");
 	        loginPage.checkRememberMe();
